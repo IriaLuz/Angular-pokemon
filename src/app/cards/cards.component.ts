52faed398fec10fbe44913a36bd858dedc8a0524
iriasuarez <iria.luz-suarez@and.digital>
2022-11-21 18:08:56 +0000

    feat(cards): delete unnecessary types

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,6 +1,5 @@
 import { PokemonDataService } from './../services/pokemonData.service';
 import { Component, OnInit } from '@angular/core';
-import { AllPokemonsType, NameAndUrl } from '../card/card';
 
 @Component({
   selector: 'app-cards',
@@ -21,10 +20,10 @@ export class CardsComponent implements OnInit {
 
   getPokemons(): void {
     this.PokemonDataService.getAllPokemons(12, this.page).subscribe(
-      (response: AllPokemonsType) => {
+      (response) => {
         this.totalPokemons = response.count;
 
-        response.results.forEach((result: NameAndUrl) => {
+        response.results.forEach((result) => {
           this.pokemonNames.push(result.name);
           console.log(this.pokemonNames);
         });
