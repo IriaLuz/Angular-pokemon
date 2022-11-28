d04d146b16b03f0573cb75d63c2fa6e16a595a83
iriasuarez <iria.luz-suarez@and.digital>
2022-11-28 18:14:59 +0000

    test: write test in description page

--- src/app/description-page/description-page.component.spec.ts
+++ src/app/description-page/description-page.component.spec.ts
@@ -1,11 +1,15 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { pokemonMockTransformed } from '../services/mockData';
 import { PokemonDataService } from '../services/pokemonData.service';
 
 import { DescriptionPageComponent } from './description-page.component';
 
 describe('DescriptionPageComponent', () => {
+  let spy: jasmine.Spy;
   let service: PokemonDataService;
   let component: DescriptionPageComponent;
   let fixture: ComponentFixture<DescriptionPageComponent>;
@@ -13,17 +17,24 @@ describe('DescriptionPageComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [DescriptionPageComponent],
-      providers: [PokemonDataService],
       imports: [RouterTestingModule, HttpClientTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(DescriptionPageComponent);
     component = fixture.componentInstance;
     service = TestBed.inject(PokemonDataService);
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set pokemon from the servive directly', () => {
+    spy = spyOn(service, 'getPokemonData').and.returnValue(
+      of(pokemonMockTransformed)
+    );
+    fixture.detectChanges();
+    expect(component.pokemon.weight).toBe(69);
+    expect(spy).toHaveBeenCalledTimes(1);
   });
 });
