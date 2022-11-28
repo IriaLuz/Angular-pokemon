9259c013ee3b5dd89dbba276328cbb1f04b3c353
iriasuarez <iria.luz-suarez@and.digital>
2022-11-28 14:05:08 +0000

    feat: refactor css in page description and update cypress test in cards

--- cypress/e2e/cards.cy.ts
+++ cypress/e2e/cards.cy.ts
@@ -11,11 +11,22 @@ describe('in my home page', () => {
     cy.contains('Next');
   });
 
+  it('Dynamic test', () => {
+    cy.intercept('GET', 'https://pokeapi.co/api/v2/pokemon?limit=12&offset=0', {
+      fixture: 'pokemon-first-page.json',
+    });
+    cy.get('app-card').first().contains('Bulbasaur');
+  });
+
   it('Click the "Previus" button on the first page', () => {
     cy.contains('Previous').click();
   });
 
   it('Click the "Next" button on the first page', () => {
     cy.contains('Next').click();
+  });
+
+  it('Click the "Learn More button" on the card', () => {
+    cy.contains('Learn More').click();
   });
 });
