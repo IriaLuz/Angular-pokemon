c0e2a1e086c097de4d6a6af400e7caa24f05a36a
iriasuarez <iria.luz-suarez@and.digital>
2022-11-29 16:48:26 +0000

    test: add test to toolbar and add cypress test to cards

--- cypress/e2e/cards.cy.ts
+++ cypress/e2e/cards.cy.ts
@@ -3,15 +3,43 @@ describe('in my home page', () => {
     cy.visit('http://localhost:4200/');
   });
 
-  it('Finds the "Previous" link', () => {
-    cy.contains('Previous');
+  it('has disabled "Previous" link in the fist page', () => {
+    cy.visit('/');
+    const findInPage = () => {
+      cy.get('li:has(span:contains(Previous))').then((element) => {
+        // do your test
+        if (element.hasClass('disabled')) {
+          // on last page, break out
+          return;
+        }
+        cy.wrap(element).click();
+        findInPage();
+      });
+    };
+    findInPage();
+  });
+
+  it('has disabled "Next" link in the last page', () => {
+    cy.visit('/');
+    const findInPage = () => {
+      cy.get('li:has(span:contains(Next))').then((element) => {
+        // do your test
+        if (element.hasClass('disabled')) {
+          // on last page, break out
+          return;
+        }
+        cy.wrap(element).click();
+        findInPage();
+      });
+    };
+    findInPage();
   });
 
   it('Finds the "Next" link', () => {
     cy.contains('Next');
   });
 
-  it('Dynamic test', () => {
+  it('shows Wulbasaur as first pokemon in the first page', () => {
     cy.intercept('GET', 'https://pokeapi.co/api/v2/pokemon?limit=12&offset=0', {
       fixture: 'pokemon-first-page.json',
     });
@@ -22,7 +50,7 @@ describe('in my home page', () => {
     cy.get('app-card').first().contains('Wulbasaur');
   });
 
-  it('Dynamic test 2', () => {
+  it('has Teedle as the first pokemon in the second page after clicking in "Next" link', () => {
     cy.intercept(
       'GET',
       'https://pokeapi.co/api/v2/pokemon?limit=12&offset=12',
@@ -37,7 +65,7 @@ describe('in my home page', () => {
     cy.get('app-card').first().contains('Teedle');
   });
 
-  it('Dynamic test 3', () => {
+  it('navigates to pokemon page description after clicking in "Learn More" button', () => {
     cy.intercept('GET', 'https://pokeapi.co/api/v2/pokemon?limit=12&offset=0', {
       fixture: 'pokemon-first-page.json',
     });
@@ -45,6 +73,6 @@ describe('in my home page', () => {
       fixture: 'pokemon-1.json',
     });
     cy.contains('Learn More').click();
-    cy.get('app-description-page').first().contains('Wulbasaur');
+    cy.get('app-description-page').first().contains('Poison');
   });
 });
--- src/app/toolbar/toolbar.component.spec.ts
+++ src/app/toolbar/toolbar.component.spec.ts
@@ -1,22 +1,46 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CardsComponent } from './../cards/cards.component';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
 
+import { Location } from '@angular/common';
+import { RouterTestingModule } from '@angular/router/testing';
 import { ToolbarComponent } from './toolbar.component';
+import { Router } from '@angular/router';
+import { routes } from '../app-routing.module';
 
 describe('ToolbarComponent', () => {
-  let component: ToolbarComponent;
+  let router: Router;
+  let location: Location;
+  // let component: ToolbarComponent;
   let fixture: ComponentFixture<ToolbarComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ToolbarComponent],
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule.withRoutes(routes)],
+      declarations: [ToolbarComponent, CardsComponent],
     }).compileComponents();
 
+    router = TestBed.inject(Router);
+    location = TestBed.inject(Location);
+
     fixture = TestBed.createComponent(ToolbarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    router.initialNavigation();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should render "HOME" link in the toolbar', () => {
+    const link =
+      fixture.debugElement.nativeElement.querySelector('#homeLink').textContent;
+    expect(link).toContain('HOME');
+    console.log(link);
   });
+
+  it('should click in "Home" link and navigate to home page', fakeAsync(() => {
+    router.navigate(['']);
+    tick();
+    expect(location.path()).toBe('/');
+  }));
 });
