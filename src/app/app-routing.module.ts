feb735468a0e37fc5d798ac2a90c48099f4ab0e0
iriasuarez <iria.luz-suarez@and.digital>
2022-12-01 10:06:06 +0000

    feat: delete unnecesary path in app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,6 @@ import { NotFoundPageComponent } from './not-found-page/not-found-page.component
 export const routes: Routes = [
   { path: 'pokemon/:name', component: DescriptionPageComponent },
   { path: '', component: CardsComponent },
-  { path: '404', component: NotFoundPageComponent },
   { path: '**', component: NotFoundPageComponent },
 ];
 
