c5c5d3e3b502c425788ee570e2ad53b198e64e4d
iriasuarez <iria.luz-suarez@and.digital>
2022-12-08 10:08:34 +0000

    feat: install fontawesome and add search-bar component to the toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { AppRoutingModule } from './app-routing.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
@@ -13,13 +14,24 @@ import { ToolbarComponent } from './toolbar/toolbar.component';
 import { DescriptionPageComponent } from './description-page/description-page.component';
 import { ButtonComponent } from './button/button.component';
 import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 @NgModule({
-  declarations: [AppComponent, CardComponent, CardsComponent, ToolbarComponent, DescriptionPageComponent, ButtonComponent, NotFoundPageComponent],
+  declarations: [
+    AppComponent,
+    CardComponent,
+    CardsComponent,
+    ToolbarComponent,
+    DescriptionPageComponent,
+    ButtonComponent,
+    NotFoundPageComponent,
+    SearchBarComponent,
+  ],
   imports: [
     NgxPaginationModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FontAwesomeModule,
   ],
   providers: [PokemonDataService],
   bootstrap: [AppComponent],
